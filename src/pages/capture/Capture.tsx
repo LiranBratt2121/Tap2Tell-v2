5a6a1379184503fbe3009f36b71b42f3216b0366
LiranBratt2121 <brattlirannin@gmail.com>
2024-12-15 12:05:12 +0200

    Add camera-capture component

--- src/pages/capture/Capture.tsx
+++ src/pages/capture/Capture.tsx
@@ -1,12 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { letters } from '../dashboard/utils';
+import { IoMdReturnLeft } from "react-icons/io";
+import { Camera, CameraProps, CameraType } from 'react-camera-pro';
+import { Back, Button, CameraContainer, CameraView } from './styles.capture';
 
 const Capture: React.FC = () => {
     const { letter } = useParams();
+    const [aspectRatio, setAspectRatio] = useState<number>(16 / 9);
     const [errors, setErrors] = useState(false);
-
+    
     const navigate = useNavigate();
+
+    const cameraRef = useRef<CameraType>(null);
+
+    useEffect(() => {
+        const updateAspectRatio = () => {
+            const { innerWidth, innerHeight } = window;
+            const ratio = innerWidth / innerHeight;
+
+            console.log(`ratio ${ratio}`)
+            if (ratio > 1.75) {
+                setAspectRatio(2.3);
+                return;
+            }
+
+            setAspectRatio(ratio > 0.6 ? (16 / 9) : (9 / 16));
+        };
+
+        updateAspectRatio();
+
+        window.addEventListener('resize', updateAspectRatio);
+        window.addEventListener('orientationchange', updateAspectRatio);
+
+        return () => {
+            window.removeEventListener('resize', updateAspectRatio);
+            window.removeEventListener('orientationchange', updateAspectRatio);
+        };
+    }, []);
 
     useEffect(() => {
         if (!letter || !(letters as string[]).includes(letter)) {
@@ -17,12 +48,42 @@ const Capture: React.FC = () => {
 
     if (errors) {
         return null;
-    } 
+    }
+
+    const videoConstraints: Omit<CameraProps, "errorMessages"> = {
+        facingMode: 'environment',
+        aspectRatio: aspectRatio,
+    };
+
+    const handleCapture = () => {
+        const screenshot = cameraRef.current?.takePhoto('base64url') as string;
+        console.log(screenshot)
+    };
+
 
     return (
-        <>
-            <h1>letter: {letter}</h1>
-        </>
+        <CameraContainer>
+            <CameraView>
+                <Camera
+                    ref={cameraRef}
+                    aspectRatio={videoConstraints.aspectRatio}
+                    facingMode={videoConstraints.facingMode}
+                    errorMessages={{}}
+                />
+            
+            <Back
+                onClick={() => navigate("/dashboard")}
+            >
+                    <IoMdReturnLeft />
+            </Back>
+            
+            <Button
+                onClick={handleCapture}
+            >
+
+            </Button>
+            </CameraView>
+        </CameraContainer>
     );
 };
 
