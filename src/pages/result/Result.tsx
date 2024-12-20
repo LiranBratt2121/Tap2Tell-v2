d5a60b09105c2cbd977d135d5a27e42ed12fff71
LiranBratt2121 <brattlirannin@gmail.com>
2024-12-20 23:32:19 +0200

    Start result page

---
+++ src/pages/result/Result.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const Result = () => {
+    const { imgLink } = useParams();
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Result
