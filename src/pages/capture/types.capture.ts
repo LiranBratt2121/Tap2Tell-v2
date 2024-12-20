a058b12d7d6373fc6b9aecad8d03b6b0d4db45a3
LiranBratt2121 <brattlirannin@gmail.com>
2024-12-20 23:31:53 +0200

    Add Result type

---
+++ src/pages/capture/types.capture.ts
@@ -0,0 +1,9 @@
+import { Letters } from "../../components/letterBox/types.letterBox";
+
+type Results = {
+    chosenCharacter: Letters;
+    detectedCharacter: Letters;
+    isRight: boolean;
+}
+
+export type { Results };
