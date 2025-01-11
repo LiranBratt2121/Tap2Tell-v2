288d3c29557d3387e12bddc2fbbd0fe9225f09ab
LiranBratt2121 <brattlirannin@gmail.com>
2025-01-11 02:27:45 +0200

    Add skyConfetti as wait gif

--- frontend/src/protectedRoute.tsx
+++ frontend/src/protectedRoute.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { WaitGif } from "./pages/result/styles.result";
+import skyConfettiGif from "./assets/skyconfetti.gif";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
@@ -15,7 +17,7 @@ const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
     }, []);
 
     if (isAuthenticated === null) {
-        return <p>Loading...</p>;
+        return <WaitGif src={skyConfettiGif} />;
     }
 
     if (!isAuthenticated) {
