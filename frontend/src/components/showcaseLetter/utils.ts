c37dc00723691a17b86e1817486b8fe1b42275db
LiranBratt2121 <brattlirannin@gmail.com>
2025-01-11 23:49:26 +0200

    Add letterImages object

---
+++ frontend/src/components/showcaseLetter/utils.ts
@@ -0,0 +1,47 @@
+import Alef from "../../assets/media/Alef.png";
+import Bet from "../../assets/media/Bet.png";
+import Gimel from "../../assets/media/Gimel.png";
+import Dalet from "../../assets/media/Dalet.png";
+import He from "../../assets/media/He.png";
+import Vav from "../../assets/media/Vav.png";
+import Zayin from "../../assets/media/Zayin.png";
+import Het from "../../assets/media/Het.png";
+import Tet from "../../assets/media/Tet.png";
+import Yod from "../../assets/media/Yod.png";
+import Kaf from "../../assets/media/Kaf.png";
+import Lamed from "../../assets/media/Lamed.png";
+import Mem from "../../assets/media/Mem.png";
+import Nun from "../../assets/media/Nun.png";
+import Samech from "../../assets/media/Samech.png";
+import Ayin from "../../assets/media/Ayin.png";
+import Peh from "../../assets/media/Peh.png";
+import Tsadeh from "../../assets/media/Tsadeh.png";
+import Qof from "../../assets/media/Qof.png";
+import Resh from "../../assets/media/Resh.png";
+import Shin from "../../assets/media/Shin.png";
+import Tav from "../../assets/media/Tav.png";
+
+export const letterImages = {
+    Alef,
+    Bet,
+    Gimel,
+    Dalet,
+    He,
+    Vav,
+    Zayin,
+    Het,
+    Tet,
+    Yod,
+    Kaf,
+    Lamed,
+    Mem,
+    Nun,
+    Samech,
+    Ayin,
+    Peh,
+    Tsadeh,
+    Qof,
+    Resh,
+    Shin,
+    Tav,
+};
