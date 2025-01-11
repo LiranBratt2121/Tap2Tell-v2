2604020fdb753c5ca2b79ee1a1ca78a70bb02aec
LiranBratt2121 <brattlirannin@gmail.com>
2025-01-11 02:27:56 +0200

    Implement Ask model

--- frontend/src/firebase/askModel.ts
+++ frontend/src/firebase/askModel.ts
@@ -1,15 +1,39 @@
-import { Letters } from "../components/letterBox/types.letterBox";
-import { Results } from "../pages/capture/types.capture";
+import { Prediction, Results } from "../pages/capture/types.capture";
+import axios from 'axios';
 
-const askModle = async (storageUrl: string): Promise<Results> => {
-    const chosenCharacter: Letters = "Gimel";
-    const detectedCharacter: Letters = "Gimel";
+const getTop3 = (predictions: Prediction[]) => {
+    const top3 = [];
 
-    return {
-        chosenCharacter: chosenCharacter,
-        detectedCharacter: detectedCharacter,
-        isRight: chosenCharacter === detectedCharacter
+
+    for (let i = 0; i < 2; i++) {
+        top3.push(predictions[i]);
     }
+
+    return top3;
 }
 
-export default askModle;
+const askModel = async (imageUrl: string) => {
+    const config = {
+        method: 'post',
+        url: 'https://us-central1-tap2-fc536.cloudfunctions.net/process_image_function',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: { image_url: imageUrl },
+    };
+
+    console.log("IMAGE URL: " + imageUrl);
+    const result = await axios.request(config).then((res) => res.data);
+    console.log(`RESULT:\n ${result}`);
+    const predictions = result["result_b64"]["predictions"];
+    const top3 = getTop3(predictions);
+    const detectedCharacter = top3[0];
+
+    return {
+        detectedCharacter,
+        top3,
+    };
+};
+
+
+export default askModel;
