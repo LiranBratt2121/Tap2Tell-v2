27df9994a74b8e6425cecb2bfe4182e5903305e9
LiranBratt2121 <brattlirannin@gmail.com>
2025-01-18 01:42:05 +0200

    Add play result sound util

--- frontend/src/pages/result/utils.ts
+++ frontend/src/pages/result/utils.ts
@@ -1,12 +1,21 @@
 import { Letters } from "../../components/letterBox/types.letterBox";
 import { Results } from "../capture/types.capture";
+import { additionalAssets } from "../../components/showcaseLetter/utils";
 
-export const isRight = (results: Results | null, trueCharacter: Letters | null) => {
+export const isRight = (results: Results | null, trueCharacter: Letters | null): boolean => {
     if (!results) {
-        return null;
+        return false;
     }
     
     const top3 = results.top3;
 
     return top3.some((curr) => curr.class === trueCharacter); 
 }
+
+export const playResultSound = (isRight: boolean) => {
+    const sound = isRight ? additionalAssets.Success : additionalAssets.Failure;
+    const resultAudio = new Audio(sound);
+    resultAudio.play();
+
+    return resultAudio;
+};
