0415600a1c43f83bed3cd23165e7d4fc1a702baa
Liran <brattlirannin@gmail.com>
2025-02-16 01:24:07 +0200

    Add styles for guide video

---
+++ frontend/src/pages/guide/styles.guide.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Sky from "../../assets/sky.png"
+
+export const MainContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    background-image: url(${Sky});
+    background-size: cover;
+`;
+
+export const GuideContainer = styled.video`
+    height: 100vh;
+    width: 100vw;
+`;
+
+export const Header = styled.h1`
+    padding: 0.3rem;
+    font-size: ${props => props.theme.fontSizes.medium};
+    color: ${props => props.theme.colors.text};
+    background-color: ${props => props.theme.colors.primary};
+    border-radius: 1rem;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
+    text-shadow: 2px 2px 2px black;
+    width: 50%;
+    display: inline-block;
+    position: absolute;
+    left: 50%;
+    top: 2%;
+    text-align: center;
+    transform: translate(-50%, -50%);
+`
