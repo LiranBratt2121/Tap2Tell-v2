ed1d2150559e863a8b49db82904b3705d973eebf
Liran <brattlirannin@gmail.com>
2025-02-15 05:28:53 +0200

    Update main

--- server/python/functions/main.py
+++ server/python/functions/main.py
@@ -1,10 +1,14 @@
 import json
+import os
 import requests
 import base64
 from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
-from core.image_processor import ImageProcessor
-from core.roboflow_model import RoboflowModel
+from core.processor import ImageProcessor
+from core.ultralytics_model import UltralyticsModel
+import numpy as np
+from PIL import Image
+from io import BytesIO
 
 initialize_app()
 
@@ -40,7 +44,7 @@ def process_image_function(req: https_fn.Request) -> https_fn.Response:
     try:
         data = req.get_json()
         image_url = data.get("image_url")
-        print("IMAGE URL: " + image_url)
+        # print("IMAGE URL: " + image_url)
         if not image_url:
             return https_fn.Response(
                 "Missing 'image_url' in request body.",
@@ -56,28 +60,20 @@ def process_image_function(req: https_fn.Request) -> https_fn.Response:
 
     # Fetch the image from the URL and convert to base64
     try:
-        print("trying to request")
-        response = requests.get(image_url, stream=True)
-        if response.status_code != 200:
-            return https_fn.Response(
-                f"Error fetching image from URL: {response.status_code}",
-                status=400,
-                headers={},  # No need to manually set CORS headers here
-            )
+        image_bytes = base64.b64decode(image_url.split(",")[1])
 
-        # Make sure the image data is processed properly
-        image_data = response.content
-        image_b64 = base64.b64encode(image_data).decode("utf-8", "ignore")
+        # Convert bytes to numpy array
+        image = Image.open(BytesIO(image_bytes))
+        image_np = np.array(image)
 
         # Process the image
         processor = ImageProcessor()
-        model = RoboflowModel()
-
-        processed_image_b64 = processor.process_image(image_b64)
-        results = model.classify(processed_image_b64)
-
+        model = UltralyticsModel()
+        
+        processed_img = processor.process_image(image_np)
+        results = model.classify(processed_img)
         return https_fn.Response(
-            response=json.dumps({"result_b64": results}),
+            response=json.dumps({"result_b64": {model.model.names.get(idx): float(conf) for idx, conf in zip(results[0].probs.top5, results[0].probs.top5conf)}}),
             status=200,
             mimetype="application/json",
             headers={},  # No need to manually set CORS headers here
