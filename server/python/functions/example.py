40466bef905eac09167bfa39ae3a8d22c8cbe4d8
Liran <brattlirannin@gmail.com>
2025-02-16 01:49:43 +0200

    Update example file

--- server/python/functions/example.py
+++ server/python/functions/example.py
@@ -2,25 +2,39 @@ import cv2
 from cv2 import imread
 import numpy as np
 from core.ultralytics_model import UltralyticsModel
+from core.processor import ImageProcessor
 from PIL import Image  
 
 def detect_objects(image: np.ndarray):
     model = UltralyticsModel()
-
-    results = model.classify(image)
+    detector = ImageProcessor()
+    
+    results = model.classify(detector.process_image(image))
     print(results[0])
     print(results[0].probs)
     
     print({model.model.names.get(i): float(conf) for i, conf in zip(results[0].probs.top5, results[0].probs.top5conf)})
     print(results[0].probs.top5)
+    
 def main():
-    im = imread("functions/core/he.png")
+    cap = cv2.VideoCapture(0)
     
-    if im is None:
-        print("Error: Image not found or cannot be loaded.")
-        return
-    
-    detect_objects(im)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        cv2.imshow('Frame', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+        to_capture = frame.copy()
 
+    cap.release()
+    cv2.destroyAllWindows()
+
+    detect_objects(to_capture)
+        
 if __name__ == "__main__":
     main()
