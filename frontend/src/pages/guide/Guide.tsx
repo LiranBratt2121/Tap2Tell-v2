8ab092782b528ccc73e89a9dced6f64c31caea3b
Liran <brattlirannin@gmail.com>
2025-02-16 00:13:57 +0200

    Add basic guide page

---
+++ frontend/src/pages/guide/Guide.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+import { GuideContainer, Header, MainContainer } from './styles.guide'
+import { videoAssets } from '../../components/showcaseLetter/utils'
+
+const Guide = () => {
+    const navigate = useNavigate()
+
+    return (
+        <MainContainer>
+            <Header>איך משחקים</Header>
+            <GuideContainer autoPlay={true} onEnded={() => navigate('/dashboard')}>
+                <source src={videoAssets.Guide} type="video/mp4" />
+            </GuideContainer>
+        </MainContainer>
+    )
+}
+
+export default Guide
