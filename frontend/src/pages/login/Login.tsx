4026ed39ee94e09ede725d973bc986c296e2662e
Liran <brattlirannin@gmail.com>
2025-03-22 22:43:19 +0200

    Add login new user logic

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -2,10 +2,12 @@ import { FormContainer, LoginContainer, Title } from './styles.login';
 import GoogleButton from 'react-google-button';
 import signInWithGoogle from '../../firebase/signInWithGoogle';
 import { useNavigate } from 'react-router-dom';
+import { getAdditionalUserInfo } from 'firebase/auth';
+import { fetchUserInformation } from '../../firebase/UserInformation';
 
 const Login = () => {
   const navigate = useNavigate();
-
+  
   return (
     <LoginContainer>
       <FormContainer>
@@ -13,7 +15,16 @@ const Login = () => {
         <GoogleButton
           label='הכנס עם גוגל'
           type="dark"
-          onClick={() => signInWithGoogle().then(() => navigate("/guide"))}
+          onClick={() => signInWithGoogle().then(async (res) => {
+            const additionUserInfo = getAdditionalUserInfo(res);
+            const currentRole = await fetchUserInformation().then(data => data?.role);
+
+            if (additionUserInfo?.isNewUser || currentRole === null) {
+              navigate("/register");
+            } else {
+              navigate("/guide");
+            }
+          })}
         />
       </FormContainer>
     </LoginContainer>
