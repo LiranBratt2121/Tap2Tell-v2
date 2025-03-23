6d654ac58f9d22a7f723eaf92c9612e2ec1e24ea
Liran <brattlirannin@gmail.com>
2025-03-23 02:13:55 +0200

    Add performance card

---
+++ frontend/src/pages/adminDashboard/components/PerformenceCard.tsx
@@ -0,0 +1,71 @@
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardContent,
+    Label,
+    ProgressContainer,
+    ProgressBar,
+    ProgressFill,
+    ProgressValue,
+    Table,
+    TableHead,
+    TableRow,
+    TableHeaderCell,
+    TableCell
+} from '../adminDashboard.styles';
+import { LettersData } from '../AdminDashboard.types';
+import { calculateOverallAccuracy } from '../utils';
+
+interface PerformanceCardProps {
+    userData: LettersData;
+}
+
+const PerformanceCard: React.FC<PerformanceCardProps> = ({ userData }) => {
+    const accuracy = calculateOverallAccuracy(userData);
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Performance Metrics</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <ProgressContainer>
+                    <Label>Overall Accuracy</Label>
+                    <ProgressBar>
+                        <ProgressFill style={{ width: `${accuracy}%` }} />
+                    </ProgressBar>
+                    <ProgressValue>{accuracy}%</ProgressValue>
+                </ProgressContainer>
+
+                <div>
+                    <Label>Letter Performance</Label>
+                    <div style={{ overflowX: 'auto' }}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableHeaderCell>Letter</TableHeaderCell>
+                                    <TableHeaderCell align="center">Accuracy</TableHeaderCell>
+                                    <TableHeaderCell align="center">Correct</TableHeaderCell>
+                                    <TableHeaderCell align="center">Total</TableHeaderCell>
+                                </TableRow>
+                            </TableHead>
+                            <tbody>
+                                {Object.entries(userData).map(([letter, data]) => (
+                                    <TableRow key={letter}>
+                                        <TableCell bold={true}>{letter}</TableCell>
+                                        <TableCell align="center">{data.accuracy}%</TableCell>
+                                        <TableCell align="center">{data.correct}</TableCell>
+                                        <TableCell align="center">{data.total}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default PerformanceCard;
