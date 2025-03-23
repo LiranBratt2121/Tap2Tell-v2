dfa23a16f93a424c5ac9c522f2414459851e0305
Liran <brattlirannin@gmail.com>
2025-03-23 02:12:48 +0200

    wip

---
+++ frontend/src/pages/adminDashboard/AdminDashboard.tsx
@@ -0,0 +1,19 @@
+// import { getAuth } from 'firebase/auth';
+// import { Container } from './adminDashboard.styles';
+// import ActivityCard from './components/ActivityCard';
+// import PerformanceCard from './components/PerformenceCard';
+// import ProfileCard from './components/ProfileCard';
+
+// const UserDashboard = () => {
+//   const auth = getAuth();
+
+//   return (
+//     <Container>
+//       <ProfileCard userData={data} />
+//       <ActivityCard userData={data} />
+//       <PerformanceCard userData={data} />
+//     </Container>
+//   );
+// };
+
+// export default UserDashboard;
