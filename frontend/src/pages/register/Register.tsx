97d505a2044bc887cfd558e2df405f25664c2b53
Liran <brattlirannin@gmail.com>
2025-04-28 12:46:27 +0300

    Improve logic to be dynamic

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,16 +1,25 @@
 import { useNavigate } from 'react-router-dom';
-import { Button, FormContainer, Header, RadioContainer, RadioImage, RegisterContainer, Title } from './styles.register';
-import { imageAssets } from '../../components/showcaseLetter/assetManger';
 import { useState } from 'react';
+import { Button, FormContainer, Header, RadioContainer, RadioImage, RegisterContainer, Title, Text } from './styles.register';
+import { imageAssets } from '../../components/showcaseLetter/assetManger';
 import { updateUserInformation } from '../../firebase/UserInformation';
 import { UserRole } from './types.register';
+
+const roles: { type: UserRole, label: string, image: string }[] = [
+    { type: 'student', label: 'תלמיד', image: imageAssets.Student },
+    { type: 'teacher', label: 'מורה', image: imageAssets.Teacher },
+];
 
 const Register = () => {
     const [selectedRole, setSelectedRole] = useState<UserRole>('');
     const navigate = useNavigate();
 
+    const handleRoleClick = (role: UserRole) => {
+        setSelectedRole(prevRole => prevRole === role ? '' : role);
+    };
+
     const handleContinue = async () => {
-        if (selectedRole === '') {
+        if (!selectedRole) {
             alert('בחר סוג משתמש');
             return;
         }
@@ -20,10 +29,16 @@ const Register = () => {
         if (success) {
             navigate('/guide');
         } else {
-            navigate('/login');
             alert('הייתה בעיה בהרשמה, נסה שוב');
+            navigate('/login');
         }
-    }
+    };
+
+    const getStyle = (role: UserRole, isText: boolean=false) => ({
+        opacity: !selectedRole || selectedRole === role ? 1 : 0.5,
+        transform: selectedRole === role && isText ? 'scale(1.05)' : selectedRole === role ? 'scale(1.1)' : 'scale(1)', // For text, smaller scale.
+        transition: 'all 0.3s ease',
+    });
 
     return (
         <RegisterContainer>
@@ -31,23 +46,18 @@ const Register = () => {
                 <Title>ברוך הבא</Title>
 
                 <Header>סוג משתמש</Header>
+
                 <RadioContainer>
-                    <RadioImage src={imageAssets.Student}
-                        onClick={() => setSelectedRole(prevRole => prevRole === 'student' ? '' : 'student')}
-                        style={{
-                            opacity: selectedRole === '' || selectedRole === 'student' ? 1 : 0.5,
-                            transform: selectedRole === 'student' ? 'scale(1.1)' : 'scale(1)',
-                            transition: 'all 0.3s ease',
-                        }}
-                    />
-                    <RadioImage src={imageAssets.Teacher}
-                        onClick={() => setSelectedRole(prevRole => prevRole === 'teacher' ? '' : 'teacher')}
-                        style={{
-                            opacity: selectedRole === '' || selectedRole === 'teacher' ? 1 : 0.5,
-                            transform: selectedRole === 'teacher' ? 'scale(1.1)' : 'scale(1)',
-                            transition: 'all 0.3s ease',
-                        }}
-                    />
+                    {roles.map(({ type, label, image }) => (
+                        <div key={type}>
+                            <RadioImage
+                                src={image}
+                                onClick={() => handleRoleClick(type)}
+                                style={getStyle(type)}
+                            />
+                            <Text style={getStyle(type, true)}>{label}</Text>
+                        </div>
+                    ))}
                 </RadioContainer>
 
                 <Button onClick={handleContinue}>
@@ -55,7 +65,7 @@ const Register = () => {
                 </Button>
             </FormContainer>
         </RegisterContainer>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
