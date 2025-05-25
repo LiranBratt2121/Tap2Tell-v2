b52b39af81aa81a4d100fcaff121d7cb4b7a157c
Liran <brattlirannin@gmail.com>
2025-05-25 17:57:48 +0300

    Add error messages

--- frontend/src/pages/capture/types.capture.ts
+++ frontend/src/pages/capture/types.capture.ts
@@ -5,4 +5,11 @@ type Prediction = {
     confidence: number;
 }
 
+export const cameraErrorMessages = {
+    noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
+    permissionDenied: 'Permission denied. Please refresh and give camera permission.',
+    switchCamera: 'It is not possible to switch camera to a different one because there is only one video device accessible.',
+    canvas: 'Canvas is not supported.'
+};
+
 export type { Prediction };
