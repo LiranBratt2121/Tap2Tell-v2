0e698a0ff77393a2a395ba7c7df28969d9b81529
Liran <brattlirannin@gmail.com>
2025-06-10 02:47:13 +0300

    Add admin mail util

---
+++ frontend/src/firebase/isAdminMail.ts
@@ -0,0 +1,12 @@
+import { getAuth } from "firebase/auth";
+import { adminMails } from "../pages/adminDashboard/adminDashboard.constants";
+
+export const isAdminMailConnected = (): boolean => {
+    const email = getAuth().currentUser?.email;
+    if (!email) {
+        console.warn("No user is currently authenticated.");
+        return false;
+    }
+
+    return adminMails.includes(email);
+};
