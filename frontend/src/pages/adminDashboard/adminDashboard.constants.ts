ec681fbf40ceee1579b48675432cb4d122ee4a5f
Liran <brattlirannin@gmail.com>
2025-06-10 02:47:50 +0300

    Add constants

---
+++ frontend/src/pages/adminDashboard/adminDashboard.constants.ts
@@ -0,0 +1,31 @@
+export const adminMails = [
+    "brattlirannin@gmail.com",
+    "shashbash2025@gmail.com",
+    "avivajac@gmail.com",
+    "taps2tell@gmail.com"
+];
+
+export const HEBREW_LETTER_NAMES: { [key: string]: string } = {
+  'Alef': 'א',
+  'Bet': 'ב',
+  'Gimel': 'ג',
+  'Dalet': 'ד',
+  'He': 'ה',
+  'Vav': 'ו',
+  'Zayin': 'ז',
+  'Het': 'ח',
+  'Tet': 'ט',
+  'Yod': 'י',
+  'Kaf': 'כ',
+  'Lamed': 'ל',
+  'Mem': 'מ',
+  'Nun': 'נ',
+  'Samech': 'ס',
+  'Ayin': 'ע',
+  'Peh': 'פ',
+  'Tsadeh': 'צ',
+  'Qof': 'ק',
+  'Resh': 'ר',
+  'Shin': 'ש',
+  'Tav': 'ת'
+};
