597cbd83ae4182787f610bde736e781a8f4aa893
Liran <brattlirannin@gmail.com>
2025-08-02 14:25:23 +0300

    Add basic back button

---
+++ frontend/src/components/buttons/BackButton.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { SendButton } from "./SendButton";
+
+export const BackButton = styled(SendButton)`
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+
+    width: auto;
+    height: auto;
+`
