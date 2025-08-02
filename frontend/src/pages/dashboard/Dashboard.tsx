88053c65f6c316be4b264d01d6037975f4b7f7d3
Liran <brattlirannin@gmail.com>
2025-08-02 13:36:26 +0300

    Add settings button

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-import { Container, DashboardContainer, Header, LettersContainer } from './styles.dashboard'
+import { Container, DashboardContainer, Header, LettersContainer} from './styles.dashboard'
 import { letters } from './utils'
+import { CiSettings } from "react-icons/ci";
 import LetterBox from '../../components/letterBox/LetterBox'
 import { AdminDashboardManager } from '../adminDashboard/manager'
 import { isAdminMailConnected } from '../../firebase/isAdminMail'
@@ -8,6 +9,7 @@ import { Button } from "../../pages/register/styles.register"
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import setTaps2tellLanguage from '../../i18n/setLanguage'
+import SettingsButton from './components/SettingsButton';
 
 const Dashboard: React.FC = () => {
   const navigate = useNavigate();
@@ -24,6 +26,7 @@ const Dashboard: React.FC = () => {
   return (
     <Container>
       <DashboardContainer>
+        <SettingsButton onClick={() => navigate("/settings")} />
         <LettersContainer>
           <Header>{t("chooseLetter")}</Header>
           {letters.map((l) => <LetterBox letter={l} key={l} />)}
