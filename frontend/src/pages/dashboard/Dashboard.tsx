b20e1ca5456711a313a880b72a69a162b59a7417
Liran <brattlirannin@gmail.com>
2025-08-02 14:06:26 +0300

    Update component

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react'
 import { Container, DashboardContainer, Header, LettersContainer} from './styles.dashboard'
 import { letters } from './utils'
-import { CiSettings } from "react-icons/ci";
 import LetterBox from '../../components/letterBox/LetterBox'
 import { AdminDashboardManager } from '../adminDashboard/manager'
 import { isAdminMailConnected } from '../../firebase/isAdminMail'
-import { Button } from "../../pages/register/styles.register"
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import setTaps2tellLanguage from '../../i18n/setLanguage'
 import SettingsButton from './components/SettingsButton';
+import { SendButton } from '../../components/buttons/SendButton';
 
 const Dashboard: React.FC = () => {
   const navigate = useNavigate();
@@ -32,7 +31,7 @@ const Dashboard: React.FC = () => {
           {letters.map((l) => <LetterBox letter={l} key={l} />)}
         </LettersContainer>
       </DashboardContainer>
-      {isAdminMailConnected() && <Button onClick={() => navigate("/adminDashboard")}>{t("adminDashboard")}</Button>}
+      {isAdminMailConnected() && <SendButton onClick={() => navigate("/adminDashboard")}>{t("adminDashboard")}</SendButton>}
     </Container>
   );
 }
